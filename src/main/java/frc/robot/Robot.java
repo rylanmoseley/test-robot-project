9cf6a9dcc3922fd2a89d578b040f4a68fd6cf3bc
Rylan Moseley <rylan.moseley@revrobotics.com>
2025-07-18 11:49:14 -0500

    update formatting?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
